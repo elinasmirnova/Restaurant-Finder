594345a1277994b20241a0d3036f84a84a930d95
elinasmirnova <smirneli@fel.cvut.cz>
2020-06-08 04:41:41 +0200

    Updates

---
+++ precache-manifest.68cedf96d6e9bb557029092b65fbaa73.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6cbf938148018acddceb6a08bfc7bd",
+    "url": "/Restaurant-Finder/index.html"
+  },
+  {
+    "revision": "be7a2a887e9f4e3bd03e",
+    "url": "/Restaurant-Finder/static/css/main.3a83290e.chunk.css"
+  },
+  {
+    "revision": "1ed66331b5214dfe218c",
+    "url": "/Restaurant-Finder/static/js/2.11840b20.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/Restaurant-Finder/static/js/2.11840b20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7a2a887e9f4e3bd03e",
+    "url": "/Restaurant-Finder/static/js/main.34569855.chunk.js"
+  },
+  {
+    "revision": "9e265ac0b01785ccfa86",
+    "url": "/Restaurant-Finder/static/js/runtime-main.564270f1.js"
+  },
+  {
+    "revision": "40f507d23b75544bdeb9634b00b6a43a",
+    "url": "/Restaurant-Finder/static/media/background.40f507d2.jpg"
+  },
+  {
+    "revision": "c0f6d507ae53204cc329176696711e0d",
+    "url": "/Restaurant-Finder/static/media/food.c0f6d507.svg"
+  },
+  {
+    "revision": "5f096cfa3feb26d9f8298a712a6776d9",
+    "url": "/Restaurant-Finder/static/media/location-pin.5f096cfa.svg"
+  },
+  {
+    "revision": "15ffdca4da395b99352870a92397ded4",
+    "url": "/Restaurant-Finder/static/media/no-image-found.15ffdca4.png"
+  },
+  {
+    "revision": "be4a3c9db730cccdd1a1e887d142620b",
+    "url": "/Restaurant-Finder/static/media/restaurant-building.be4a3c9d.svg"
+  },
+  {
+    "revision": "09e204efd6735a1efa594bedb1e746c6",
+    "url": "/Restaurant-Finder/static/media/star.09e204ef.svg"
+  },
+  {
+    "revision": "695c076ae1a4a8fcaeb99cf2dcbba041",
+    "url": "/Restaurant-Finder/static/media/video-bg.695c076a.mp4"
+  }
+]);
